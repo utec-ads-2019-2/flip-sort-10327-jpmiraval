1ab36b1c374db356efa74fa921fc63fb04087e98
unknown <jean.miraval@utec.edu.pe>
2019-08-29 18:55:06 -0500

    Ahora si

--- main.cpp
+++ main.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
-void Flip(int &num1, int &num2) {
-    int temp = num1;
-    num1 = num2;
-    num2 = temp;
-}
+
 
 int main() {
 
-    int largoLista,  contador=0;
-while(cin>>largoLista){
-    int lista[largoLista];
-
-    for(int i = 0; i<largoLista; i++){
-        cin>>lista[i];
-    }
-
-    for(int i = 0; i<largoLista; i++){
-        for(int k = i; k <largoLista;k++){
-            if(lista[i] > lista[k]){
-                Flip( lista[k] , lista[i]);
-                contador++;
-            }
+    int arrayLenght;
+    while(cin>>arrayLenght) {
+        int array[arrayLenght];
+        int index=0;
+        int contador=0;
+        while(index<arrayLenght){
+            cin>>array[index];
+            index++;
         }
+        for(size_t i=0;i<arrayLenght-1;i++){
+            for(size_t j=0;j<arrayLenght-1-i;j++){
+                if(array[j]>array[j+1]){
+                    swap(array[j],array[j+1]);
+                    contador++;}
+            }}
+        cout<<"Minimum exchange operations : "<< contador<<endl;
+
     }
-
-    cout << "Minimun exchange operations :" << contador;
-}
-
-
-
-
-
     return 0;
 }
